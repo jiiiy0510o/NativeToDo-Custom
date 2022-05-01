dba92cfd4ed19b0de0a3f2951f77e3239feb85f7
Young <jiiiy0510o@gmail.com>
2022-05-01 23:57:22 +0900

    Touchable관련 공부

---
+++ App.js
@@ -0,0 +1,36 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity, TouchableHighlight, TouchableNativeFeedback } from "react-native";
+import { theme } from "./colors";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Text style={styles.btnText}>Work</Text>
+        </TouchableOpacity>
+        <TouchableNativeFeedback onPress={() => console.log("pressed")}>
+          <Text style={styles.btnText}>Travel</Text>
+        </TouchableNativeFeedback>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+  },
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 100,
+  },
+  btnText: {
+    color: "white",
+    fontSize: 42,
+  },
+});
